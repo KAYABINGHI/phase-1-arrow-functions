615e4c870500b2293065e3bd9c862c9b78a492e2
KAYABINGHI <muchaipauls@gmail.com>
2025-06-22 16:39:34 +0300

    arrow-function

---
+++ index.js
@@ -0,0 +1,10 @@
+//Function expression called divide that divides 2000 by 100
+const divide = function () {
+    return 2000/100
+} 
+
+//Arrow function called square that takes one parameter and multiplies it by itself
+const square = (num) => num * num;
+
+//Arrow function called add that takes two parameters and adds them together
+const add = (a, b) => a + b;
